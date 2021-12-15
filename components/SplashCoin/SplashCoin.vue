3ed5f64642ac8f7cca5e877cec8b89fbc4b03a42
bdanielmr <bdanielmr@gmail.com>
2021-12-15 00:42:30 -0500

    add component-splash

---
+++ components/SplashCoin/SplashCoin.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="splash-coin">
+    <a>
+      {{ `Money ` }}<span>{{ `Exchange` }}</span></a
+    >
+    <nuxt-link to="home"><button>Start</button></nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SplashCoin',
+}
+</script>
+
+<style lang="sass">
+.splash-coin
+    width: 100%
+    height: 100vh
+    display: flex
+    flex-direction: column
+    align-items: center
+    justify-content: center
+    font-size: 3em
+    font-weight: 800
+    a
+        color: white
+    button
+        width: 250px
+        height: 50px
+        background: white
+        margin: 10px
+        cursor: pointer
+        font-size: 16px
+        font-weight: 600
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,17 @@
 <template>
-  <Tutorial />
+  <SplashCoin />
 </template>
 
 <script>
+import SplashCoin from '~/components/SplashCoin/SplashCoin.vue'
 export default {
   name: 'IndexPage',
+  components: { SplashCoin },
 }
 </script>
+<style lang="sass">
+html, body
+  margin: 0
+  padding: 0
+  background:  linear-gradient(180deg, #4A28AF 0%, #6E46E6 100%)
+</style>
